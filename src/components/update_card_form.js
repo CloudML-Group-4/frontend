eb67c63765727f26ed2b81636ee5c01f67904bdc
ari <aricerrahyan@gmail.com>
2023-04-13 14:56:56 -0400

    update business card component added

---
+++ src/components/update_card_form.js
@@ -0,0 +1,35 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './update_card_form.css'
+
+export default function UpdateCardForm () {
+    return(
+        <div className="UpdateCardForm" class="container bg-dark text-light p-3">
+            <form action="" method="post">
+                <div class="mb-3">
+                    <label for="lead_name" class="form-label">Lead Name</label>
+                    <input type="text" class="form-control" id="lead_name" name="lead_name"></input>
+                </div>
+                <div class="mb-3">
+                    <label for="phone_number" class="form-label">Phone Number</label>
+                    <input type="tel" class="form-control" id="phone_number" name="phone_number" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"></input>
+                    <small>Format: 123-456-7890</small>
+                </div>
+                <div class="mb-3">
+                    <label for="email_address" class="form-label">Email Address</label>
+                    <input type="email" class="form-control" id="email_address" name="email_address"></input>
+                </div>
+                <div class="mb-3">
+                    <label for="company_website" class="form-label">Company Website</label>
+                    <input type="text" class="form-control" id="company_website" name="company_website"></input>
+                </div>
+                <div class="mb-3">
+                    <label for="company_address" class="form-label">Company Address</label>
+                    <input type="text" class="form-control" id="company_address"></input>
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+    )
+}
---
+++ src/pages/update_card.js
@@ -0,0 +1,7 @@
+import UpdateCardForm from "../components/update_card_form";
+
+export default function UpdateCard () {
+    return(
+        <UpdateCardForm />
+    )
+}
