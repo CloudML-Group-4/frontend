703da67dff73286f530b4d2fdea56a7ea49d11fa
ari <aricerrahyan@gmail.com>
2023-04-13 15:09:10 -0400

    moved business card form component into its own folder

--- src/pages/update_card.js
+++ src/pages/update_card.js
@@ -1,4 +1,4 @@
-import UpdateCardForm from "../components/update_card_form";
+import UpdateCardForm from "../components/update_card_form/update_card_form";
 
 export default function UpdateCard () {
     return(
